bc85dafd4bb016759e740387933a87a5e2f869ff
anfaas1618 <Anfaas@123>
2021-11-12 19:16:45 +0530

    1.code formatting 2.Box class code complete actions(set position, clear last box, draw box, do set clear draw using single function)

--- main.cpp
+++ main.cpp
@@ -1,25 +1,69 @@
 #include <iostream>
 #define WIDTH 50
-#define HEIGHT 15
+#define HEIGHT 50
+#include <thread>
+#include <Windows.h>
+#include <unistd.h>
 using namespace std;
-void draw();
-void init();
-char arr[HEIGHT][WIDTH];
-int main(int len,char** args){
-    init();
-    arr[(HEIGHT/2)-1][(WIDTH/2)-1]='#';
-    draw();
+static int code=0;
+void exit(int code)
+{
+    ::code=code;
+}
+void start();
+void update();
+void ShowConsoleCursor(bool showFlag)
+{
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    return  0;
+    CONSOLE_CURSOR_INFO     cursorInfo;
 
-}void  draw(){
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-           cout<<arr[i][j];
-        }
-        cout<<endl;
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag; // set the cursor visibility
+    SetConsoleCursorInfo(out, &cursorInfo);
+}
+int main(){
+    start();
+    while (!code==1)
+    {
+        update();
     }
-}void  init(){
+
+}
+char arr[HEIGHT][WIDTH];
+class Box {
+public:
+    int COORDINATES[4][2]{{0,0},{0,1},{1,0},{1,1}};
+    void setxy(int x , int y)
+    {
+        for (int i = 0; i < 4; ++i) {
+            COORDINATES[i][0]+=x;
+            COORDINATES[i][1]+=y;
+        }
+    }
+    void place()
+    {
+        for (int i = 0; i < 4; ++i) {
+            arr[COORDINATES[i][0]][COORDINATES[i][1]]='#';
+        }
+    }
+    void clear()
+    {
+        for (int i = 0; i < 4; ++i) {
+            arr[COORDINATES[i][0]][COORDINATES[i][1]]=' ';
+        }
+    }
+     void setClearPlace(int x,int y)
+    {
+        for (int i = 0; i < 4; ++i) {
+            arr[COORDINATES[i][0]][COORDINATES[i][1]]=' ';
+            COORDINATES[i][0]+=x;
+            COORDINATES[i][1]+=y;
+            arr[COORDINATES[i][0]][COORDINATES[i][1]]='#';
+        }
+    }
+};
+void  init(){
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             if(i==0||i==HEIGHT-1)
@@ -30,4 +74,29 @@ int main(int len,char** args){
         }
 
     }
+}
+void  draw(){
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+}
+void start(){
+    init();
+    Box box;
+    ShowConsoleCursor(false);
+    draw();
+    box.setxy(3,3);
+    box.place();
+    draw();
+    box.clear();
+    draw();
+    box.setClearPlace(5,5);
+    draw();
+    getchar();
+}
+void update(){
+    exit(0);
 }
